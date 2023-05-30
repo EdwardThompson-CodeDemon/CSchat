26b3221359627c765ca475df3bc88a4b1a5e1571
stacynganga <Centuaries@254>
2023-05-30 10:04:49 +0300

    member scan

---
+++ app/src/main/java/sparta/realm/cschat/activities/MemberSearch.java
@@ -0,0 +1,14 @@
+package sparta.realm.cschat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_search);
+    }
+}
